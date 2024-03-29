4c564400f6194724c258187a1f6d716288e754ba
Dev-98 <devparker48@gmail.com>
2024-03-29 20:34:24 +0530

    vector data upload  pipeline

--- Data_upload.py
+++ Data_upload.py
@@ -3,6 +3,7 @@ import requests, os, random, string
 import pandas as pd
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 hf_token = os.environ.get('HF_TOKEN')
@@ -37,10 +38,44 @@ def upsert_data(text: str,company: str,stipend: str,JobTitles,skills,link,desc,n
 
     print("Vector upserted successfully")
 
+def job_data(urls):
+    
+    skills = []
+    descriptions = []
+    cnames = []
+    titles = []
+#     location = []
+    stipend = []
+    
+    for url in urls:
+        try :
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            cnames.append(soup.find("div", class_ = 'company_and_premium').text.strip())
+    #         location.append(soup.find("span", class_ = 'location_link view_detail_button').text.strip())
+
+            titles.append(soup.find("h1", class_ = 'heading_2_4 heading_title').text.strip())
+            skill = soup.find("div", class_ = "round_tabs_container").text.strip().replace("\n"," , ")
+            skills.append(skill)
+            descriptions.append(soup.find("div", class_ = "text-container").text.strip()+ f". {skill}")
+            stipend.append(soup.find("span", class_='stipend').text.strip())
+        except Exception as e:
+            print(f"Error with : {url}. Skipping this one")
+            continue
+        
+    data = zip(cnames,titles,skills,descriptions,stipend,urls)
+    with open('JobData.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Company_Name', 'JobTitles', 'Skills', 'Description','Stipend','Links'])  # Write header
+        writer.writerows(data)
+
+    
 
 if __name__ == '__main__':
     csv_path = 'newdata.csv'
     data = pd.read_csv(csv_path)
+    # job_data(links)
     for i,desc in enumerate(data['Description']) :
           print(i)
           upsert_data(desc,data['Company_Name'][i],data['Stipend'][i],data['JobTitles'][i],data['Skills'][i],data['Links'][i],data['Description'][i],"internship")
